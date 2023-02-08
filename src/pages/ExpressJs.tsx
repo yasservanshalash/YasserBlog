6c547c2d6da456389dfb7e9d92228567be199eb0
Yasser Shalash <yasservanshalash@gmail.com>
2023-02-08 18:31:59 +0100

    added routers and controllers

--- src/pages/ExpressJs.tsx
+++ src/pages/ExpressJs.tsx
@@ -77,19 +77,13 @@ if(id === 0 || id > notes.length) {
   }
 }
 })
-// app.post("/:id", (req: Request, res: Response) => {
-//   const newNote = {id: Number(req.params.id), title: "new note", content: "content"}
-//   notes.push(newNote)
-//   res.json(notes)
-// });
+
 app.post('/notes', (req: Request, res: Response) => {
   const newId = notes[notes.length - 1].id + 1
   const newNote = Object.assign({id: newId}, req.body)
   notes.push(newNote);
   res.status(201).json(notes);
 });
-
-
 
 app.delete("/notes/:id", (req: Request, res: Response) => {
   if(+req.params.id === 0 || +req.params.id > notes.length) {
@@ -108,90 +102,107 @@ app.put("/notes/:id", (req: Request, res: Response) => {
       <Typography>
         You can also use the router middleware by initializing a router. i.e. const noteRouter = Express.Router(); The app will look like this
       </Typography>
-      <Highlight>
-        {`import Express, { Request, Response } from "express";
+      <Highlight className="javascript">
+        {`// index.ts
+import Express, {Request, Response} from "express";
+import userRouter from './routes/user'
 
 const app = Express();
-app.use(Express.json());
 
-const notes = [
+app.use(Express.json());
+app.use("/users", userRouter)
+
+const port = 8000
+app.listen(port, () => {
+    console.log("Express server listening on port " + port)
+})`}
+      </Highlight>
+      <Highlight className="javascript">
+        {`// in userController.ts
+import { Request, Response } from "express";
+
+const userList = [
   {
     id: 1,
-    title: "HTML",
-    content: "Learn HTML for webpage structure",
+    name: "Yasser",
+    age: 26,
+    email: "yasservanshalash@gmail.com",
   },
   {
     id: 2,
-    title: "CSS",
-    content: "Learn CSS for webpage styling",
+    name: "Foo",
+    age: 27,
+    email: "foo@bar.com",
   },
   {
     id: 3,
-    title: "JS",
-    content: "Learn HTML for webpage structure",
+    name: "Jane",
+    age: 19,
+    email: "Jane@doe.com",
   },
   {
     id: 4,
-    title: "React",
-    content: "Learn React the Javascript Library",
+    name: "John",
+    age: 33,
+    email: "John@doe.com",
   },
 ];
 
-const noteRouter = Express.Router();
-app.use("/notes", noteRouter);
-
-const getNotes = (req: Request, res: Response) => {
-  if (notes.length === 0) {
-    res.status(404).json("No resources found");
-  } else {
-    res.status(200).json(notes);
+export const getUsers = async (req: Request, res: Response) => {
+  try {
+    res.json(userList);
+  } catch (error) {
+    console.log(error);
   }
 };
 
-const createNote = (req: Request, res: Response) => {
-  const newId = notes[notes.length - 1].id + 1;
-  const newNote = Object.assign({ id: newId }, req.body);
-  notes.push(newNote);
-  res.status(201).json(notes);
+export const createUser = async (req: Request, res: Response) => {
+  try {
+    const newUser = req.body;
+    userList.push(newUser);
+    res.json(userList);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-const getNote = (req: Request, res: Response) => {
-  const id = +req.params.id;
-  if (id === 0 || id > notes.length) {
-    res.status(404).json("No resources found");
-  } else {
-    const note = notes.find((note) => note.id === Number(id));
-    if (note) {
-      res.status(200).json(note);
-    } else {
-      res.status(404).json("resource not found");
-    }
+export const deleteUser = async (req: Request, res: Response) => {
+  try {
+    const userId = req.params.id;
+    const result = userList.filter((user) => user.id !== +userId);
+    res.json(result);
+  } catch (error) {
+    console.log(error);
   }
-}
+};
 
-const deleteNote = (req: Request, res: Response) => {
-  if (+req.params.id === 0 || +req.params.id > notes.length) {
-    res.status(404).json("Resource doesnt exist");
-  } else {
-    const result = notes.filter((note) => note.id !== Number(req.params.id));
-    res.status(200).json(result);
+export const updateUser = async (req: Request, res: Response) => {
+  try {
+    const userId = req.params.id;
+    userList[+userId - 1] = req.body;
+    res.json(userList);
+  } catch (error) {
+    console.log(error);
   }
-}
-
-const updateNote = (req: Request, res: Response) => {
-  notes[Number(req.params.id) - 1] = req.body;
-  res.status(201).json(notes);
-}
-
-noteRouter.route("/").get(getNotes).post(createNote);
-noteRouter.route("/:id").get(getNote).delete(deleteNote).put(updateNote);
-
-
-const port = 4000;
-app.listen(port, () => {
-  console.log("Example app listening on port 4000!");
-});
+};
 `}
+      </Highlight>
+      <Highlight className="javascript">
+        {` // in user Router
+import { Router } from "express";
+import { getUsers, createUser, deleteUser, updateUser } from "../controllers/user";
+
+const router = Router();
+
+router.get("/", getUsers)
+
+router.post("/", createUser)
+
+router.delete("/:id", deleteUser)
+
+router.put("/:id", updateUser)
+
+export default router`}
       </Highlight>
     </Box>
   );
